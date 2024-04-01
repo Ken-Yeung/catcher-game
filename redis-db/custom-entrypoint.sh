ce6286e1a55e5e4aa02441981a27c172656f3f97
ogcken <109201673+ogcken@users.noreply.github.com>
2024-04-01 22:58:15 +0800

    switching to dev enviornment

--- redis-db/custom-entrypoint.sh
+++ redis-db/custom-entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p /usr/local/etc/redis
 echo "aclfile /usr/local/etc/redis/custom_aclfile.acl" > /usr/local/etc/redis/redis.conf
 
 # Generate ACL file using environment variables
-echo "user admin on allkeys allchannels allcommands >$REDIS_PW" > /usr/local/etc/redis/custom_aclfile.acl
+echo "user $REDIS_USER on allkeys allchannels allcommands >$REDIS_PW" > /usr/local/etc/redis/custom_aclfile.acl
 
 # Call the original Docker entrypoint script with redis-server and the path to the custom Redis configuration
 exec docker-entrypoint.sh redis-server /usr/local/etc/redis/redis.conf
