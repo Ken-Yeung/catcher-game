8ff211d9a78f46d7ebb281221c507b04a87f83d4
ogcken <109201673+ogcken@users.noreply.github.com>
2024-04-04 15:25:30 +0800

    enabled production mode

--- react-fe/src/App.tsx
+++ react-fe/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <div>Hello World</div>
+      <div>Hello World</div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
